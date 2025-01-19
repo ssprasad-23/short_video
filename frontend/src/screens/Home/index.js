b1d836a9f48cfa6990948a84a8fbb6871c645888
ssprasad-23 <shynal_007@yahoo.com>
2025-01-19 00:19:12 -0800

    inital cleanup

---
+++ frontend/src/screens/Home/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {View, Text} from "react-native";
+
+
+const Home = () => {
+  return (
+    <View>
+      <Text>Home world</Text>
+    </View>
+  );
+}
+
+export default Home;
