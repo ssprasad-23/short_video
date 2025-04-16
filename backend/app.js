d91aebdab6fa6ea5ad2d14253c0ee7b4a9a0f04c
ssprasad-23 <shynal_007@yahoo.com>
2025-04-15 19:28:36 -0700

    db config

--- backend/app.js
+++ backend/app.js
@@ -3,9 +3,7 @@ import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from '@apollo/server/express4'; 
 import { typeDefs } from "./src/graphql/schemas.js";
 import { resolvers } from "./src/graphql/resolvers.js";
-import sql from "mysql2";
 import dotenv from "dotenv";
-
 
 const app = express();
 app.use(express.json());
@@ -19,23 +17,6 @@ const server = new ApolloServer({
   resolvers,
 });
 
-//Create mysql connection
-const db = sql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
-
-//Connect to the database
-db.connect((err) => {
-  if (err) {
-    console.error("Error connecting to database:", err);
-    return;
-  }
-  console.log("Connected to database.");
-});
-
 //Start apollo server
 async function startServer() {
   await server.start();
@@ -45,5 +26,6 @@ async function startServer() {
     console.log(`Express server running at http://localhost:${PORT}/`);
     console.log(`GraphQL server running at http://localhost:${PORT}/graphql`);
   });
-}
+};
+
 startServer();
