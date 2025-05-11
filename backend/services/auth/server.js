b7c033540e7e15a92b7184bb2ad5b1189cbb2fb7
ssprasad-23 <shynal_007@yahoo.com>
2025-05-11 03:42:27 -0700

    connect to postgres db

--- backend/services/auth/server.js
+++ backend/services/auth/server.js
@@ -1,15 +1,19 @@
-import express from 'express';
+import { Client } from 'pg';
 import dotenv from 'dotenv';
 
-
-// Initialize Express
-const app = express();
-app.use(express.json());
 dotenv.config();
 
-const PORT = process.env.PORT || 3002;
-
-// Start server
-app.listen(PORT, () => {
-  console.log(`Express server running at http://localhost:${PORT}/`);
+const db = new Client({
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT || 5432
 });
+
+// Connect to the database
+db.connect()
+    .then(() => {
+        console.log("Connected to PostgreSQL");
+    })
+    .catch(err => {
+        console.error("Error connecting to PostgreSQL:", err.stack);
+    });
